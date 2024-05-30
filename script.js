c843a4ee8ff419f0ad95089074b53bc21795bf22
DDD <163750523+DanieleTrippodo@users.noreply.github.com>
2024-05-30 14:16:20 +0200

    Base Scaffolding

---
+++ script.js
@@ -0,0 +1,19 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+        // todo - Qui vi vanno tutti i dati
+    };
+},
+
+
+// # funzioni VUE ------------------------------------------------------------------------------------------
+
+methods: {
+    
+    // todo - Qui vanno inserite tutte le funzioni
+
+}
+
+}).mount('#app');
